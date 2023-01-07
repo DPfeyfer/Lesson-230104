39614261a99586e5883562db2516f56ac33c802f
DPfeyfer <Skydenfer737@gmail.com>
2023-01-07 13:23:58 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,32 @@ public class Main {
 
         //task1
         System.out.println("Task 1");
+        for (byte i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
 
 
         //task2
         System.out.println("Task 2");
+        for (byte i = 10; i >= 0; i--) {
+            System.out.println(i);
+        }
 
 
         //task3
         System.out.println("Task 3");
+        for (int i = 2; i < 17; i = i + 2) {
+            System.out.println(i);
+        }
 
 
         //task4
         System.out.println("Task 4");
+        for (byte i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
+
+
 
 
         //task5
