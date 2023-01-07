767c1d02dcf4eb3ffec1ac8db8b555b02bcdf8b3
DPfeyfer <Skydenfer737@gmail.com>
2023-01-07 13:01:04 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,46 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        //task1
+        System.out.println("Task 1");
+
+
+        //task2
+        System.out.println("Task 2");
+
+
+        //task3
+        System.out.println("Task 3");
+
+
+        //task4
+        System.out.println("Task 4");
+
+
+        //task5
+        System.out.println("Task 5");
+
+
+        //task6
+        System.out.println("Task 6");
+
+
+        //task7
+        System.out.println("Task 7");
+
+
+        //task8
+        System.out.println("Task 8");
+
+
+        //task9
+        System.out.println("Task 9");
+
+
+        //task10
+        System.out.println("Task 10");
+
+
     }
 }
